3624c6927654391fdc5bdb048b7eb1b9b172a2cc
Giancarlo Palavicini <kasthor@gmail.com>
2020-03-10 17:30:53 -0600

    fix: enable access files

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ CREDENTIALS_PATH=/root/.s3fs
 echo $AWS_ACCESS_KEY:$AWS_SECRET_KEY > $CREDENTIALS_PATH
 chmod 600 $CREDENTIALS_PATH
 
-s3fs $BUCKET_NAME /var/www -o nonempty -o passwd_file=$CREDENTIALS_PATH
+s3fs $BUCKET_NAME /var/www -o nonempty -o passwd_file=$CREDENTIALS_PATH -o use_cache=/tmp/.s3fs-$BUCKET_NAME
 
 nginx -g 'daemon off;'
