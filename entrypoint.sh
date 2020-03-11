1d694f12bdd14eb969a8907f1eab03b5c671fc21
Giancarlo Palavicini <kasthor@gmail.com>
2020-03-10 20:15:38 -0600

    fix: file access permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ CREDENTIALS_PATH=/root/.s3fs
 echo $AWS_ACCESS_KEY:$AWS_SECRET_KEY > $CREDENTIALS_PATH
 chmod 600 $CREDENTIALS_PATH
 
-s3fs $BUCKET_NAME /var/www -o nonempty -o passwd_file=$CREDENTIALS_PATH -o use_cache=/tmp/.s3fs-$BUCKET_NAME
+s3fs $BUCKET_NAME /var/www -o nonempty -o passwd_file=$CREDENTIALS_PATH -o use_cache=/tmp/.s3fs-$BUCKET_NAME -o umask=0000 -o uid=`id -u nginx` -o allow_other
 
 nginx -g 'daemon off;'
